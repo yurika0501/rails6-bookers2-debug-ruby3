bfddc59284eb5292e473a2e186d75bdb892571c5
EC2 Default User <ec2-user@ip-172-31-47-250.ap-northeast-1.compute.internal>
2023-02-07 05:41:00 +0000

    [Add]比率表実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,12 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true,length:{ maximum: 200 }
   
+  # scope :スコープの名前, -> { 条件式 }
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }#今週 
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }#前週
+  
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
